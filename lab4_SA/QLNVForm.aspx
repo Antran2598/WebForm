14c149c01f7f2453fdc43aaf22c20729e8d05363
An Tran <dinhan2509@gmail.com>
2021-11-28 13:35:52 +0700

    Web_Form

---
+++ lab4_SA/QLNVForm.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="QLNVForm.aspx.cs" Inherits="lab4_SA.QLNVForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="SEARCH" OnClick="Button1_Click" />
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="true" OnSelectedIndexChanged="GridView1_SelectedIndexChanged"></asp:GridView>
+        <br/>
+        <table>
+        <tr>
+            <td>MaNV</td> 
+            <td><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+        <td>Ten</td> 
+        <td><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+        <td>Chucvu</td> 
+        <td><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+        <td>Phongban</td> 
+        <td><asp:TextBox ID="TextBox5" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr>
+        <td>Chuthich</td> 
+        <td><asp:TextBox ID="TextBox6" runat="server"></asp:TextBox></td>
+        </tr>
+        </table><br />
+        <asp:Button ID="Button2" runat="server" Text="ADD" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Text="UPDATE" OnClick="Button3_Click" />
+        <asp:Button ID="Button4" runat="server" Text="DELETE" OnClick="Button4_Click" OnClientClick="return confirm('ARE U SURE');" />
+    </form>
+</body>
+</html>
---
+++ lab4_SA/QLNVForm.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace lab4_SA
+{
+    public partial class QLNVForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<ChitietNV> nhanviens = new NV_BUS().laydanhsach();
+            GridView1.DataSource = nhanviens;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String keyword = TextBox1.Text.Trim();
+            List<ChitietNV> nhanviens = new NV_BUS().Search(keyword);
+            GridView1.DataSource = nhanviens;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int Manv = int.Parse(GridView1.SelectedRow.Cells[1].Text.Trim());
+            ChitietNV nhanvien =new NV_BUS().Getdetails(Manv);
+            if(nhanvien !=null)
+            {
+                TextBox2.Text = nhanvien.Manv.ToString();
+                TextBox3.Text = nhanvien.Ten;
+                TextBox4.Text = nhanvien.Chucvu;
+                TextBox5.Text = nhanvien.Phongban;
+                TextBox6.Text = nhanvien.Chuthich;
+            }    
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ChitietNV newnhanvien = new ChitietNV()
+            {
+                Manv = 0,
+                Ten = TextBox3.Text.Trim(),
+                Chucvu = TextBox4.Text.Trim(),
+                Phongban = TextBox5.Text.Trim(),
+                Chuthich = TextBox6.Text.Trim()
+            };
+
+            bool result = new NV_BUS().Addnew(newnhanvien);
+            if(result)
+            {
+                List<ChitietNV> nhanviens = new NV_BUS().laydanhsach();
+                GridView1.DataSource = nhanviens;
+                GridView1.DataBind();
+            }    
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            int Manv = int.Parse(TextBox2.Text.Trim());
+            bool result = new NV_BUS().Delete(Manv);
+            if (result)
+            {
+                List<ChitietNV> nhanviens = new NV_BUS().laydanhsach();
+                GridView1.DataSource = nhanviens;
+                GridView1.DataBind();
+            }else
+            {
+                WebMsgBox.Show("SORRY BAE");
+            }    
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            ChitietNV newnhanvien = new ChitietNV()
+            {
+                Manv = int.Parse(TextBox2.Text.Trim()),
+                Ten = TextBox3.Text.Trim(),
+                Chucvu = TextBox4.Text.Trim(),
+                Phongban = TextBox5.Text.Trim(),
+                Chuthich = TextBox6.Text.Trim()
+            };
+
+            bool result = new NV_BUS().Update(newnhanvien);
+            if (result)
+            {
+                List<ChitietNV> nhanviens = new NV_BUS().laydanhsach();
+                GridView1.DataSource = nhanviens;
+                GridView1.DataBind();
+            }
+            else
+            {
+                WebMsgBox.Show("SORRY BAE");
+            }
+        }
+    }
+}
